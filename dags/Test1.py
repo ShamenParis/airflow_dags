068f0f1ea9f96ca5b754aa6f49d90761ccad67de
ShamenParis <73017335+ShamenParis@users.noreply.github.com>
2025-03-22 00:58:59 +0000

    Test Airflow Sync

---
+++ dags/Test1.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 3, 12),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(dag_id='FirstDAG', default_args=default_args, schedule_interval='@once', catchup=False)
+
+start = DummyOperator(task_id='start', dag=dag)
+end = DummyOperator(task_id='end', dag=dag)
+
+start >> end
